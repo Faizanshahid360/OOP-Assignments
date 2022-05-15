9fdfed4fe4fd5bdcf4099376123356a50feba7d4
Faizanshahid360 <105553860+Faizanshahid360@users.noreply.github.com>
2022-05-15 16:43:02 +0500

    Add files via upload

---
+++ Assignment_1/Esc.java
@@ -0,0 +1,12 @@
+public class Esc  
+    {  
+		public static void main(String args[])   
+		{  
+		System.out.println("\tHelloWorld");  
+		System.out.println("\bHelloWorld");  
+		System.out.println("\nHow are you");  
+		System.out.println("\'Good\',Thanks");  
+		System.out.println("Hi,\"Good Morning\""); 
+		System.out.println("\\ - backslash");
+		}	
+    }
