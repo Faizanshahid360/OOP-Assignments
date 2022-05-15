cb60d31f95f050a94f0fdf3309c0fe166937366a
Faizanshahid360 <105553860+Faizanshahid360@users.noreply.github.com>
2022-05-15 16:34:26 +0500

    Add files via upload

---
+++ Assignment_1/Lit_Java.java
@@ -0,0 +1,30 @@
+public class Lit_Java  
+	{  
+	public static void main(String args[])   
+		{  
+			int count = 786;  
+			int hexaVal = 0x7e6; 
+			int octalVal = 0105; 
+			int binary = 0b11010; 
+			float floatVal = 45.99f;  
+			double cost = 176.567;   
+			char alpha = 'p';  
+			String str = "Java Literals";  
+			boolean boolVal = true;  
+			String stuName = null;  
+			char ch1 = '\u0021';  
+			char ch2 = 1456;  
+	System.out.println(count);  
+	System.out.println(floatVal);  
+	System.out.println(cost);  
+	System.out.println(hexaVal);  
+	System.out.println(binary);  
+	System.out.println(alpha);  
+	System.out.println(str);  
+	System.out.println(boolVal);  
+	System.out.println(octalVal);  
+	System.out.println(stuName);  
+	System.out.println(ch1);    
+	System.out.println(ch2);  
+	}  
+}  
