b6c2f7497d287ac9ad34f2e4aad692d95e1e23c6
Faizanshahid360 <105553860+Faizanshahid360@users.noreply.github.com>
2022-05-16 17:10:22 +0500

    Add files via upload

---
+++ Mod.java
@@ -0,0 +1,10 @@
+// Modulus "%" operator.
+public class Mod {
+ public static void main(String args[]) {
+	int x = 46;
+	double y = 46.25;
+// Modulus means remainder
+ System.out.println("x mod 6 = " + x % 6);
+ System.out.println("y mod 6 = " + y % 6);
+ }
+}
