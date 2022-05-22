e1b0d2b502c409f71ce896334fa502269a7df098
Faizanshahid360 <105553860+Faizanshahid360@users.noreply.github.com>
2022-05-22 18:10:33 +0500

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,18 @@
+public class Class {
+  public static void main(String[] args) {
+    
+    // how to use class in java
+    class User{
+    	
+    	int score;
+    }
+    
+    User faizan = new User();
+    
+    faizan.score = 20;
+    
+    System.out.println(faizan.score);
+    
+ 
+  }
+}
